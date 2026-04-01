15ab72880028967f8d048a5ebe6de38cecbbce46
Hugo Straseele <straseelehugo@gmail.com>
2026-04-01 20:41:56 +0200

    ajout de la table tasks dans le schéma de la db

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE users (
 
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
+
+CREATE TABLE tasks (
+    id SERIAL,
+    id_user INT NOT NULL,
+    title VARCHAR(100),
+    description VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT pk_task PRIMARY KEY (id),
+    CONSTRAINT fk_task FOREIGN KEY (id_user) REFERENCES users(id)
+);

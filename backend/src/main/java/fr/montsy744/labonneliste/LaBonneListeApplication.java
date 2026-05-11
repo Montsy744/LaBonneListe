545a17f6c5c601d30d629eb99a27f2314b5ff335
Hugo Straseele <straseelehugo@gmail.com>
2026-05-11 21:41:46 +0200

    init de Spring

---
+++ backend/src/main/java/fr/montsy744/labonneliste/LaBonneListeApplication.java
@@ -0,0 +1,13 @@
+package fr.montsy744.labonneliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBonneListeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBonneListeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/montsy744/laBonneListe/LaBonneListeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.montsy744.laBonneListe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBonneListeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
